a733a8bd689276669ad3971f2df0fe8ba72c5ce7
Muhammad Maaz <maazm6387@gmail.com>
2024-11-12 21:04:07 +0500

    changes in api services

--- EMS-Frontend/src/APIServices/LoginAPI.js
+++ EMS-Frontend/src/APIServices/LoginAPI.js
@@ -1,13 +1,14 @@
-// apiService.js
 import axios from 'axios';
-
-const API_BASE_URL = "http://localhost:8000";  // Replace with your backend URL
 
 export const loginUser = async (email, password) => {
   try {
-    const response = await axios.post(`${API_BASE_URL}/login`, { email, password });
+    const response = await axios.post('http://localhost:8000/login', {
+      email,
+      password
+    });
     return response.data;
   } catch (error) {
+    console.error("Login error:", error.response.data);
     throw error.response ? error.response.data : { detail: "An error occurred" };
   }
 };
